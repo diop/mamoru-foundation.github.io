273030e43cb51cf52a25639c085a1722b4beb067
b <b@bs-MacBook-Pro.local>
2023-05-06 17:11:16 +0200

    override css styles using head style

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,21 @@
 export default {
+    head: (
+        <>
+            <meta name="msapplication-TileColor" content="#fff" />
+            <meta httpEquiv="Content-Language" content="en" />
+            <meta name="description" content="Mamoru Documentation" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@shuding_" />
+            <meta property="og:title" content="Mamoru Documentation" />
+            <meta property="og:description" content="Mamoru Documentation" />
+            <meta name="apple-mobile-web-app-title" content="Mamoru" />
+            <style>{`
+                
+            `}</style>
+        </>
+    ),
     logo: <span>Mamoru Documentation</span>,
+    docsRepositoryBase: 'https://github.com/Mamoru-Foundation/mamoru-foundation.github.io/blob/main/',
     footer: {
         text: '© Copyright Mamoru.ai 2023 - MIT'
     },
@@ -9,4 +25,4 @@ export default {
     chat: {
         link: 'https://discord.gg'
     }
-  }
+}
