f3705602fefc05980a3d8e6ffd4addd6cc509192
Fabian Gutierrez <camilocorrea@ip-192-168-0-18.eu-central-1.compute.internal>
2023-05-18 05:30:25 -0500

    feat: changed basePath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx',
+
   });
 
 module.exports = withNextra()
-
+module.exports.basePath = '/docs';
 module.exports.images = {
     unoptimized: true
 };
